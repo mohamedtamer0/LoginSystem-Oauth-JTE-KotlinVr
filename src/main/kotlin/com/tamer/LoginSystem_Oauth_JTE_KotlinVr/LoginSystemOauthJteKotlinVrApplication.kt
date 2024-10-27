a83cc6c282b80b563f3d7d2398d5515cc92b0ee0
mohamedtamer0 <mt4452242@gmail.com>
2024-10-27 19:26:53 +0300

    update

---
+++ src/main/kotlin/com/tamer/LoginSystem_Oauth_JTE_KotlinVr/LoginSystemOauthJteKotlinVrApplication.kt
@@ -0,0 +1,11 @@
+package com.tamer.LoginSystem_Oauth_JTE_KotlinVr
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LoginSystemOauthJteKotlinVrApplication
+
+fun main(args: Array<String>) {
+	runApplication<LoginSystemOauthJteKotlinVrApplication>(*args)
+}
