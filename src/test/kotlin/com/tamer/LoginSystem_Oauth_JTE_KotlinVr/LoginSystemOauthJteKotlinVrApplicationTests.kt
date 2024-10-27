912e74650fd1c5a563ef2f8fe23c92a6ef66cc87
mohamedtamer0 <mt4452242@gmail.com>
2024-10-27 18:54:04 +0300

    update

---
+++ src/test/kotlin/com/tamer/LoginSystem_Oauth_JTE_KotlinVr/LoginSystemOauthJteKotlinVrApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tamer.LoginSystem_Oauth_JTE_KotlinVr
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LoginSystemOauthJteKotlinVrApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
